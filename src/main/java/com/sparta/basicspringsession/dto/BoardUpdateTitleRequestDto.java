4f58afbc445064762e5a72b5d10d126138f325f6
vege4944 <vege4944@gmail.com>
2024-08-28 13:49:17 +0900

    board 7th practice done

---
+++ src/main/java/com/sparta/basicspringsession/dto/BoardUpdateTitleRequestDto.java
@@ -0,0 +1,8 @@
+package com.sparta.basicspringsession.dto;
+
+import lombok.Getter;
+
+@Getter
+public class BoardUpdateTitleRequestDto {
+    private String title;
+}
---
+++ src/main/java/com/sparta/basicspringsession/entity/Board.java
@@ -0,0 +1,33 @@
+package com.sparta.basicspringsession.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+
+public class Board{
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String contents;
+
+    public Board(String title, String contents) {
+        this.title = title;
+        this.contents = contents;
+    }
+
+    public void updateTitle (String title) {
+        this.title = title;
+    }
+
+    public void updateContents (String contents) {
+        this.contents = contents;
+    }
+}
